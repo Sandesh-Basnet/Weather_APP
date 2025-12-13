8298f8644c434afbb33cf015c6c37bb275cb8255
Sandesh Basnet <72078439+Sandesh-Basnet@users.noreply.github.com>
2025-12-13 19:28:32 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const api_key = "1a988c5549b766ca3fdc02ac928f78cd";// OpenWeatherMap API Key
+const form = document.getElementById("weatherForm");// Form Element
+const Input_city = document.getElementById("search_location");// Input Field Element
+const search_btn = document.getElementById("search-btn");// Search Button Element
+const location_display = document.getElementById("location");// Location Display Element
+const time_display = document.getElementById("current_time");// Time Display Element
+const day_display = document.getElementById("day");// Day Display Element
+const date_display = document.getElementById("date");// Date Display Element
+const weather_icon_display = document.getElementById("weather-condition-icon");// Weather Icon Display Element
+const weather_condition_short_display = document.getElementById("weather-conditon-short");// Weather Condition Short Description Display Element
+const temp_max_display = document.getElementById("temp_max_value");// Maximum Temperature Display Element
+const temp_min_display = document.getElementById("temp_min_value");// Minimum Temperature Display Element
+const humidity_display = document.getElementById("humidity_value");// Humidity Display Element
+const wind_display = document.getElementById("wind_speed_value");// Wind Speed Display Element
+
+//Event listerner for form submission working mechanism:First when the sumbit button is pressed
+//or clicked then the event listener captures that event and triggers the associated function.
+//In this case, the function is an anonymous arrow function defined right there in the event listener.
+//Inside this function, e.preventDefault() is called to stop the browser from performing its 
+//default action of reloading the page upon form submission. This allows the script to handle 
+//the form data (in this case, fetching weather data for the entered city) without losing the 
+//current state of the page.
+form.addEventListener("submit", (e) => {// Event Listener for Form Submission
+  e.preventDefault(); // prevent page reload
+//e stands for event object, which contains information about the event (like what element triggered it, default browser behavior, etc.).
+  const city = Input_city.value.trim();// Get the city name from input field and trim removes the unnecessary spaces
+  if (!city) return;// If input is empty, do nothing
+
+  console.log("Searching weather for:", city); // for testing
+  fetchWeatherData(city);// Call function to fetch weather data
+});
+
+
+
+
+
+// fetchWeatherData("London");
+async function fetchWeatherData(city){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`);
+
+        if(!response.ok){
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+
+
